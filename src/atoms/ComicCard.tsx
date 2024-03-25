d5f81c5ab94310601cfa62a8ca4fddd941fd725c
tanadendere <tanadendere@gmail.com>
2024-03-25 08:28:57 +0200

    feat: :lipstick: Updated the comic card and added an outline for it

--- src/atoms/ComicCard.tsx
+++ src/atoms/ComicCard.tsx
@@ -1,9 +1,22 @@
 import { IComicData } from "../models/comicData";
+import { Card, CardHeader, CardBody, Image } from "@nextui-org/react";
 
 function ComicCard({ comic }: { comic: IComicData }) {
   return (
     <>
-      <div>{comic.name}</div>
+      <Card className="py-4 h-64">
+        <CardBody className="overflow-visible py-2">
+          <Image
+            alt="Card background"
+            className="object-cover rounded-xl"
+            src="/images/hero-card-complete.jpeg"
+            width={270}
+          />
+        </CardBody>
+        <CardHeader className="pb-0 pt-2 px-4 flex-col items-start">
+          <h4 className="font-bold text-large">{comic.name}</h4>
+        </CardHeader>
+      </Card>
     </>
   );
 }
